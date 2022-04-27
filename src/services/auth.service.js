ba72f5a0a474246e559d141bac689800c24f3f4e
Djebbi Amine <aminedjebbie@gmail.com>
2022-04-27 06:24:34 +0200

    final validation button

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,39 +1,18 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/home/";
+const API_URL = "http://localhost:5000/api/auth/";
 
 class AuthService {
-  login(userid, password) {
-    return axios
-      .post(API_URL + "login", { userid, password })
-      .then((response) => {
-        localStorage.setItem("user", JSON.stringify(response.data));
-        return response.data;
-      });
-  }
 
-  logout() {
-    localStorage.removeItem("user");
-  }
-/*
-  validate(userid, password) {
+
+  validate(userid) {
     return axios
-      .post(API_URL + "validate", { userid, password })
+      .post(API_URL + "validate", {userid})
       .then((response) => {
         return response.data;
       });
   }
-*/
-  register(userid,name,lastname,email,password) {
-    return axios.post(API_URL + "register", {
-      userid,
-      name,
-      lastname,
-      email,
-      password
 
-    });
-  }
 }
 
 export default new AuthService();
